8ff99a1fa1e202bc508dce9145fda8565f4f9960
kimberly palen <palenkimberly31@gmail.com>
2024-09-05 15:17:31 +0800

    initial upload for activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Student;
+use App\Models\Course;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/students/create', function () {
+    $student = new Student();
+    $student->first_name = 'Kimmy';
+    $student->last_name = 'Palen';
+    $student->age = '20';
+    $student->email = 'palenkimberly@gmail.com';
+    $student->save();
+    return 'Student Created';
+});
+
+Route::get('/students', function () {
+    $students = Student::all();
+    return $students;
+});
+
+Route::get('/students/update', function () {
+    $student = student::where('email', 'palenkimberly31@gmail.com')->first();
+    $student->email = 'kimberlypalen@gmail.com';
+    $student->save();
+    return 'Student Updated!';
+});
+
+Route::get('/students/delete', function () {
+    $student = student::where('email', 'kimberlypalen@gmail.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
+});
+
+Route::get('/courses/create', function () {
+    $course = new Course();
+    $course->course_name = 'Introduction to Databases';
+    $course->save();
+    return 'Course Created!';
+});
+
+Route::get('/course/{id}/students', function ($id) {
+    $course = Course::find($id);
+    return $course->students;
 });
